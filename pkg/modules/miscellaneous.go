95a6b42d13d6fcbc93cc1f54e783a23d0430754b
Subhajit <ajay.dutta94@gmail.com>
2025-03-18 02:26:51 +0530

    Add home route to dev tools API

--- pkg/modules/miscellaneous.go
+++ pkg/modules/miscellaneous.go
@@ -7,7 +7,9 @@ func SampleFunction(a string, b string, c string) string {
 }
 
 func HealthCheck(context *gin.Context) {
+
 	context.JSON(200, gin.H{
 		"message": "OK",
+		"sample":  SampleFunction("a", "b", "c"),
 	})
 }
