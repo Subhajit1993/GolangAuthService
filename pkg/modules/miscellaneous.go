1722e1f215aa5ece8c1c8bda55d23541a60e3c71
Subhajit <ajay.dutta94@gmail.com>
2025-03-18 02:25:51 +0530

    Add home route to dev tools API

--- pkg/modules/miscellaneous.go
+++ pkg/modules/miscellaneous.go
@@ -2,6 +2,10 @@ package modules
 
 import "github.com/gin-gonic/gin"
 
+func SampleFunction(a string, b string, c string) string {
+	return a + b
+}
+
 func HealthCheck(context *gin.Context) {
 	context.JSON(200, gin.H{
 		"message": "OK",
