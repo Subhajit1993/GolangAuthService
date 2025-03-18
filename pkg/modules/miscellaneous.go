6a8817395e6def05702b8d4f70f8fce35ad246c0
Subhajit <ajay.dutta94@gmail.com>
2025-03-18 18:30:35 +0530

    Password hardcode

--- pkg/modules/miscellaneous.go
+++ pkg/modules/miscellaneous.go
@@ -1,9 +1,13 @@
 package modules
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"os"
+)
 
 func HealthCheck(context *gin.Context) {
-
+	file, _ := os.Open("nonexistentfile.txt")
+	defer file.Close()
 	context.JSON(200, gin.H{
 		"message": "OK",
 	})
