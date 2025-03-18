96387352002c9123497027d5f0f54c19888be983
Subhajit <ajay.dutta94@gmail.com>
2025-03-18 17:40:04 +0530

    Duplicate api and SampleFunction

--- pkg/modules/miscellaneous.go
+++ pkg/modules/miscellaneous.go
@@ -2,14 +2,9 @@ package modules
 
 import "github.com/gin-gonic/gin"
 
-func SampleFunction(a string, b string, c string, d string) string {
-	return a + b
-}
-
 func HealthCheck(context *gin.Context) {
 
 	context.JSON(200, gin.H{
 		"message": "OK",
-		"sample":  SampleFunction("a", "b", "c", "d"),
 	})
 }
